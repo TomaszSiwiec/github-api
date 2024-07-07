9aaf239f6c50e153b50398fcc940d81629cd881a
s29662 <s29662@pjwstk.edu.pl>
2024-07-07 16:44:21 +0200

    added spotless

--- src/main/java/com/empik/githubapi/entity/User.java
+++ src/main/java/com/empik/githubapi/entity/User.java
@@ -1,6 +1,11 @@
 package com.empik.githubapi.entity;
 
-//FIXME star import is not allowedimport lombok.AllArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
